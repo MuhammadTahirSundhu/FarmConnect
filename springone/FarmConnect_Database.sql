d5a9134781580d0b6cd22dca830f6df8caf0a560
MuhammadTahirSundhu <tahirsundhu8956@gmail.com>
2024-11-14 15:53:27 +0500

    Successfully connect my server with the backend

---
+++ springone/FarmConnect_Database.sql
@@ -0,0 +1,4 @@
+create database farmconnect
+use farmconnect
+
+select * from Logins
---
+++ springone/src/main/java/com/example/springone/Login.java
@@ -0,0 +1,26 @@
+package com.example.springone;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Logins")
+@Data
+public class Login {
+
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+
+
+}
